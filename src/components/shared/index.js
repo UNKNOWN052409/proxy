d794c476278f64927d2bc505507e6c8a05e2dda1
UNKNOWN052409 <unknownrakesh0524@gmail.com>
2026-07-22 20:09:10 +0530

    add src/components/shared/index.js

---
+++ src/components/shared/index.js
@@ -0,0 +1,9 @@
+"use client";
+
+export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as Badge } from "./Badge";
+export { default as Toggle } from "./Toggle";
+export { default as Modal } from "./Modal";
+export { default as Skeleton } from "./Skeleton";
+export { ThemeProvider, useTheme } from "./ThemeProvider";
